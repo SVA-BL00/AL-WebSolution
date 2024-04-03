1c7e21a0385d3eb71c4389752b2a825300df4d53
SourSushi360 <jplopez365@hotmail.com>
2024-04-03 14:00:44 -0600

    changed arrows

--- flaskr/static/js/prueba.js
+++ flaskr/static/js/prueba.js
@@ -1,16 +1,32 @@
 let route = 'Scene1.jpg';
 let contenedor = document.getElementById("viewer");
 let imagen = document.createElement("img");
+let right = document.querySelector('#right');
+let left = document.querySelector('#left');
+let up = document.querySelector('#up');
+let down = document.querySelector('#down');
 imagen.src = "../static/img/" + route;
 contenedor.appendChild(imagen);
+right.style.opacity = 1;
+left.style.opacity = 0;
+up.style.opacity = 0;
+down.style.opacity = 0;
 
 function move(str) {
     console.log(str);
     if (route == 'Scene1.jpg' && str == 'right') {
         route = 'Scene2.jpg';
+        right.style.opacity = 0;
+        left.style.opacity = 0;
+        up.style.opacity = 1;
+        down.style.opacity = 0;
     }
     if (route == 'Scene2.jpg' && str == 'up') {
         route = 'Scene1.jpg';
+        right.style.opacity = 1;
+        left.style.opacity = 0;
+        up.style.opacity = 0;
+        down.style.opacity = 0;
     }
     imagen.src = "../static/img/" + route;
 }
