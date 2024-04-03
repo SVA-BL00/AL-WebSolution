0e05c43f41f0d49c2b8e47aeeffd2ae049f31396
SourSushi360 <jplopez365@hotmail.com>
2024-04-03 12:30:54 -0600

    included streetView

--- flaskr/static/js/prueba.js
+++ flaskr/static/js/prueba.js
@@ -1,3 +1,10 @@
+import {Viewer} from "@photo-sphere-viewer/core";
+
+const viewer = new Viewer ({
+    container: document.querySelector('#viewer'),
+    panorama: '../img/Scene2.jpg',
+});
+
 const nodes = [
     { id: 'node-1', panorama: '001.jpg', links: [{ nodeId: 'node-2', position: { textureX: 1500, textureY: 780 } }] },
     { id: 'node-2', panorama: '002.jpg', links: [{ nodeId: 'node-1', position: { textureX: 3000, textureY: 780 } }] },
