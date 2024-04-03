a88b7480804ad900dbfc9d09d698c53ada98b4c2
SourSushi360 <jplopez365@hotmail.com>
2024-04-03 15:12:54 -0600

    final

--- flaskr/static/js/prueba.js
+++ flaskr/static/js/prueba.js
@@ -5,12 +5,14 @@ let right = document.querySelector('#right');
 let left = document.querySelector('#left');
 let up = document.querySelector('#up');
 let down = document.querySelector('#down');
+let maria = document.querySelector('#maria')
 imagen.src = "../static/img/" + route;
 contenedor.appendChild(imagen);
 right.style.opacity = 1;
 left.style.opacity = 0;
 up.style.opacity = 0;
 down.style.opacity = 0;
+maria.style.opacity = 0;
 
 function move(str) {
     console.log(str);
@@ -20,6 +22,7 @@ function move(str) {
         left.style.opacity = 0;
         up.style.opacity = 1;
         down.style.opacity = 0;
+        maria.style.opacity = 1;
     }
     if (route == 'Scene2.jpg' && str == 'up') {
         route = 'Scene1.jpg';
@@ -27,6 +30,7 @@ function move(str) {
         left.style.opacity = 0;
         up.style.opacity = 0;
         down.style.opacity = 0;
+        maria.style.opacity = 0;
     }
     imagen.src = "../static/img/" + route;
 }
