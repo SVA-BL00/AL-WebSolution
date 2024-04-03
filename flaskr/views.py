ffe3b2832a999e42b32cf7647ed030a33fbc4321
Mireya Isabel Rodríguez Pasillas <133801089+mirp01@users.noreply.github.com>
2024-04-03 10:02:29 -0600

    db

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,95 +1,61 @@
-from flask import render_template, Blueprint, session, flash, redirect, url_for, request, flash
-import os
-import string, secrets, smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from flask import render_template, Blueprint, session, flash, redirect, url_for, request
 from werkzeug.security import check_password_hash, generate_password_hash
 import functools
 from . import get_db_connection
 
 views = Blueprint('views', __name__)
 
-## CREACIÓN DE CREDENCIALES PARA USUARIO ##
-
-def send_email(email, password):
-    subject = 'Tu información de acceso'
-    body = f'\nTu contraseña: {password}'
-
-    message = MIMEMultipart()
-    message['From'] = os.getenv('EMAIL_ADDRESS')
-    message['To'] = email
-    message['Subject'] = subject
-    message.attach(MIMEText(body, 'plain'))
-
-    with smtplib.SMTP(os.getenv('SMTP_SERVER'), os.getenv('SMTP_PORT')) as server:
-        server.starttls()
-        server.login(os.getenv('EMAIL_ADDRESS'), os.getenv('APP_PASSWORD'))
-        server.sendmail(os.getenv('EMAIL_ADDRESS'), email, message.as_string())
-
-def generate_random_password():
-    alphabet = string.ascii_letters + string.digits
-    password = ''.join(secrets.choice(alphabet) for i in range(8))
-    return password
-
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if 'USER_ID' not in session:
-            flash("Por favor ingresa a sesión")
-            return redirect(url_for('loginGET'))
-
-        return view(**kwargs)
-
-    return wrapped_view
 
 @views.route('/')
 def home():
-    return render_template("base.html")
+    return render_template("main.html")
 
-@views.get("/login")
-def login_get():
+@views.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Process form submission
+        usermail = request.form.get('userMail')
+        password = request.form.get('password')
+
+        conn = get_db_connection()
+        if not conn:
+            flash("Failed to connect to database", 'error')
+            return redirect(url_for('views.login'))
+
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM Employees WHERE Email = ?", (usermail,))
+            user = cursor.fetchone()
+
+            if user and user[5] == password:  
+                session.clear()
+                session['USER_ID'] = user[0]  
+                session.permanent = True
+                flash('¡Has ingresado correctamente!', 'success')
+                return redirect(url_for('views.home'))
+
+
+            else:
+                flash('Usuario o contraseña incorrectos', 'error')
+        except Exception as e:
+            flash('Error al intentar iniciar sesión', 'error')
+            print(f"Error: {e}")
+        finally:
+            conn.close()
+
+    # Render login form
     return render_template("login.html")
 
-@views.post("/login")
-def login_post():
-    email = request.form['usermail']
-    password = request.form['password']
+
+@views.route("/search")
+def search():
+    q = request.args.get("q")
+    print(q)
+
+    if q:
+        print(q)
+        #Nombre del table.query.filter(nombretable.nombre.icontains(q) | nombretable.nombre.icontains(q)).order_by(nombre.idk.asc()).order_by(asdasd)).limit(10).all
+    else:
+        results = []
     
-    conn = get_db_connection()
-    if not conn:
-        flash("Failed to connect to database")
-        return redirect(url_for('views.login_get'))
-    
-    try:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
-        user = cursor.fetchone()
-
-        if user and check_password_hash(user['password'], password):
-            session.clear()
-            session['USER_ID'] = user['usuario_id']
-            session.permanent = True
-            flash('¡Has ingresado correctamente!')
-            return redirect(url_for('views.home'))
-        else:
-            flash('Usuario o contraseña incorrectos', 'error')
-            return redirect(url_for('views.login_get'))
-    except Exception as e:
-        flash('Error al intentar iniciar sesión', 'error')
-        print(f"Error: {e}")
-        return redirect(url_for('views.login_get'))
-    finally:
-        conn.close()
-
-@views.route("/xd")
-def prueba():
-    return render_template("admin.html")
-
-""" @views.route('/registro', methods=['GET,POST'])
-def register():
-    if request.method=='POST':
-        #Se recupera la información del forms
-        passw = generate_random_password()
-        passwAc = generate_password_hash(passw)
-        flash('Registro exitoso, el correo con tu contraseña ha sido enviado!', 'success')
-        #send_email(correo,passw) """
+    return render_template("search_results.html", results=results)
