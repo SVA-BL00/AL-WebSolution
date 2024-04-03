73be5ba127cf9677416edee23eec225a887d8b69
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-04-02 20:50:18 -0600

    Create prueba.js

---
+++ flaskr/static/js/prueba.js
@@ -0,0 +1,4 @@
+const nodes = [
+    { id: 'node-1', panorama: '001.jpg', links: [{ nodeId: 'node-2', position: { textureX: 1500, textureY: 780 } }] },
+    { id: 'node-2', panorama: '002.jpg', links: [{ nodeId: 'node-1', position: { textureX: 3000, textureY: 780 } }] },
+];
