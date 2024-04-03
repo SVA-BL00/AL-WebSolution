8a790d8fcbe374ae790104d8bbc62e9c4b3c3141
Sara Vargas <sarav.notes@gmail.com>
2024-04-03 13:22:40 -0600

    cambios a db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,10 +1,20 @@
 import pyodbc
 import os
 from dotenv import load_dotenv
-from . import get_db_connection
 
 # Load environment variables from .env file
 load_dotenv()
 
-
-
+def get_db_connection():
+    try:
+        conn = pyodbc.connect(
+            f"DRIVER=ODBC Driver 18 for SQL Server;"
+            f"SERVER={os.getenv('DB_SERVER')};"
+            f"DATABASE={os.getenv('DB_NAME')};"
+            f"UID={os.getenv('DB_USERNAME')};"
+            f"PWD={os.getenv('DB_PASSWORD')}"
+        )
+        return conn
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        return None
